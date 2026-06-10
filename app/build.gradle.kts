1022444cd124f6a090e9baa7928d1f1c00857300
GsxrGuy80s <rhearns1980@gmail.com>
2026-06-10 08:14:16 -0400

    chore: bump version to 0.1.0-rc2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.pocketnode.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.0-rc1"
+        versionCode = 4
+        versionName = "0.1.0-rc2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
